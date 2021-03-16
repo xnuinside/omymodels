876774fcf7874202455d2e944539f9c818ea6933
xnuinside@gmail.com <xnuinside@gmail.com>
2021-03-16 19:27:00 +0300

    improve imports, defaults

--- omymodels/gino/core.py
+++ omymodels/gino/core.py
@@ -2,11 +2,11 @@ import os
 from simple_ddl_parser import DDLParser, parse_from_file
 from typing import Optional, List, Dict
 import omymodels.gino.templates as gt
-from omymodels.gino.types import types_mapping, postgresql_dialect
+from omymodels.gino.types import types_mapping, postgresql_dialect, datetime_types
 
 
 state = set()
-postgresql_dialect = set()
+postgresql_dialect_cols = set()
 
 
 def get_tables_information(
@@ -25,9 +25,14 @@ def get_tables_information(
 
 
 def prepare_column_type(column_data: Dict) -> str:
-    column_type = types_mapping.get(column_data["type"].lower(), "OMM_UNMAPPED_TYPE")
+    
+    global postgresql_dialect_cols
+    column_data_type = column_data["type"].lower().split('[')[0]
+    column_type = types_mapping.get(column_data_type, "OMM_UNMAPPED_TYPE")
+    if column_type == 'OMM_UNMAPPED_TYPE':
+        print(column_data["type"], 'OMM_UNMAPPED_TYPE')
     if column_type in postgresql_dialect:
-        postgresql_dialect.add(column_type)
+        postgresql_dialect_cols.add(column_type)
     column = gt.column_template.format(
         column_name=column_data["name"], column_type=column_type
     )
@@ -39,18 +44,28 @@ def prepare_column_type(column_data: Dict) -> str:
 
 
 def prepare_column_default(column_data: Dict, column: str) -> str:
-    if column_data["default"] == "now()":
-        column_data["default"] = "func.now()"
-        state.add("func")
-    elif isinstance(column["default"], str):
-        column_data["default"] = f"'{column_data['default']}'"
-    column += gt.default(default=column_data["default"])
+    if isinstance(column_data["default"], str):
+        if column_data["type"].upper() in datetime_types:
+            if 'now' in column_data["default"]:
+                # todo: need to add other popular PostgreSQL & MySQL functions
+                column_data["default"] = "func.now()"
+                state.add("func")
+            elif "'" not in column_data["default"]:
+                column_data["default"] = f"'{column_data['default']}'"
+        elif 'true' in column_data["default"] and not "'" in column_data["default"]:
+            column_data["default"] = True
+        elif 'false' in column_data["default"] and not "'" in column_data["default"]:
+            column_data["default"] = False
+        else:
+            if "'" not in column_data["default"]:
+                column_data["default"] = f"'{column_data['default']}'"
+    column += gt.default.format(default=column_data["default"])
     return column
 
 
 def setup_column_attributes(column_data: Dict, table_pk: List[str], column: str) -> str:
 
-    if column_data["type"].lower() == "serial":
+    if column_data["type"].lower() == "serial" or column_data["type"].lower() == "bigserial":
         column += gt.autoincrement
     if not column_data["nullable"]:
         column += gt.required
@@ -72,11 +87,18 @@ def generate_column(column_data: Dict, table_pk: List[str]) -> str:
 
 def generate_model(table: Dict) -> str:
     """ method to prepare one Model defention - name & tablename  & columns """
-    model = gt.model_template.format(
-        model_name=table["table_name"].capitalize(), table_name=table["table_name"]
-    )
-    for column in table["columns"]:
-        model += generate_column(column, table["primary_key"])
+    print(table)
+    model = ""
+    if table.get('table_name'):
+        # mean table
+        model = gt.model_template.format(
+            model_name=table["table_name"].capitalize(), table_name=table["table_name"]
+        )
+        for column in table["columns"]:
+            model += generate_column(column, table["primary_key"])
+    elif table.get('sequence_name'):
+        # create sequence
+        ...
     return model
 
 
@@ -84,9 +106,10 @@ def create_header(tables: List[Dict]) -> str:
     """ header of the file - imports & gino init """
     header = ""
     if "func" in state:
-        header += gt.sql_alchemy_func_import
-    if postgresql_dialect:
-        header += ",".join(postgresql_dialect)
+        header += gt.sql_alchemy_func_import + "\n"
+    print(postgresql_dialect)
+    if postgresql_dialect_cols:
+        header += gt.postgresql_dialect_import.format(types=",".join(postgresql_dialect_cols)) + "\n"
     header += gt.gino_import + "\n\n"
     if tables[0]["schema"]:
         header += gt.gino_init_schema + "\n"
@@ -107,7 +130,8 @@ def generate_gino_models_file(tables: List[Dict]) -> str:
 
 def save_models_to_file(models: str, dump_path: str) -> None:
     folder = os.path.dirname(dump_path)
-    os.makedirs(folder, exist_ok=True)
+    if folder:
+        os.makedirs(folder, exist_ok=True)
     with open(dump_path, "w+") as f:
         f.write(models)
 
@@ -125,3 +149,4 @@ def create_gino_models(
     else:
         print(output)
         return output
+
