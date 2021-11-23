ba8411aa4cf6111240ef795dcf915677e4e6ea78
xnuinside@gmail.com <xnuinside@gmail.com>
2021-11-23 22:32:00 +0300

    fix test

--- tests/unit/test_common.py
+++ tests/unit/test_common.py
@@ -51,7 +51,7 @@ class UsersWorkSchedule(db.Model):
 
     __tablename__ = 'users_WorkSchedule'
 
-    id = db.Column(ARRAY((1,1)), nullable=False)
+    id = db.Column(ARRAY((1,1)), primary_key=True)
     request_drop_date = db.Column(smalldatetime())
     shift_class = db.Column(db.String(5))
     start_history = db.Column(datetime2(7), nullable=False)
