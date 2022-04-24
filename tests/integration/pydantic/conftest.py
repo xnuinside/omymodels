c49b49eb4393c7f8cef9f398cae67afa2189b291
xnuinside@gmail.com <xnuinside@gmail.com>
2022-04-24 14:03:18 +0400

    add conftest for integrations tests for model generation

---
+++ tests/integration/pydantic/conftest.py
@@ -0,0 +1,30 @@
+import importlib
+import os
+import uuid
+from types import ModuleType
+from typing import Optional
+
+import pytest
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+package = ".".join(__file__.replace(os.sep, ".").split("omymodels.")[1].split(".")[:-2])
+
+
+@pytest.fixture
+def load_generated_code():
+    def _inner(code_text: str, module_name: Optional[str] = None) -> ModuleType:
+        """method saves & returns new generated python module
+        code_text - code to be saved in new module
+        module_name: str - name of the module to use for saving the code
+        """
+        if not module_name:
+            module_name = f"module_{uuid.uuid1()}"
+
+        with open(os.path.join(current_path, f"{module_name}.py"), "w+") as f:
+            f.write(code_text)
+
+        module = importlib.import_module(f"{package}.{module_name}")
+
+        return module
+
+    yield _inner
---
+++ tests/integration/pydantic/test_pydantic.py
@@ -0,0 +1,32 @@
+import os
+
+from omymodels import create_models
+
+
+def test_pydantic_models_are_working_as_expected(load_generated_code) -> None:
+    ddl = """
+    CREATE table user_history (
+            runid                 decimal(21) null
+        ,job_id                decimal(21)  null
+        ,id                    varchar(100) not null -- group_id or role_id
+        ,user              varchar(100) not null
+        ,status                varchar(10) not null
+        ,event_time            timestamp not null default now()
+        ,comment           varchar(1000) not null default 'none'
+        ) ;
+
+
+    """
+    result = create_models(ddl, models_type="pydantic")["code"]
+
+    module = load_generated_code(result)
+
+    used_model = module.UserHistory(
+        id="group_id",
+        user="user",
+        status="active",
+    )
+
+    assert used_model
+
+    os.remove(os.path.abspath(module.__file__))
