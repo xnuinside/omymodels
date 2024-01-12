6523ae78fd30d6a53aa3a6262ff8996d842ea0b4
cfhowes <cfhowes@users.noreply.github.com>
2024-01-12 09:58:15 -0800

    Foreign Key processing updates (#55)
    * Improved handling of different ways of specifing references in the DDL.
    
    * Flake fixes

--- tests/functional/generator/test_sqlalchemy.py
+++ tests/functional/generator/test_sqlalchemy.py
@@ -124,6 +124,135 @@ class Attachments(Base):
     assert result == expected
 
 
+def test_foreign_keys_defined_inline():
+    """
+    This should be the same output as test_foreign_keys, but with a slightly
+    different, yet valid input DDL.
+    """
+    expected = """import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class Materials(Base):
+
+    __tablename__ = 'materials'
+
+    id = sa.Column(sa.Integer(), primary_key=True)
+    title = sa.Column(sa.String(), nullable=False)
+    description = sa.Column(sa.String())
+    link = sa.Column(sa.String())
+    created_at = sa.Column(sa.TIMESTAMP())
+    updated_at = sa.Column(sa.TIMESTAMP())
+
+
+class MaterialAttachments(Base):
+
+    __tablename__ = 'material_attachments'
+
+    material_id = sa.Column(sa.Integer(), sa.ForeignKey('materials.id'))
+    attachment_id = sa.Column(sa.Integer(), sa.ForeignKey('attachments.id'))
+
+
+class Attachments(Base):
+
+    __tablename__ = 'attachments'
+
+    id = sa.Column(sa.Integer(), primary_key=True)
+    title = sa.Column(sa.String())
+    description = sa.Column(sa.String())
+    created_at = sa.Column(sa.TIMESTAMP())
+    updated_at = sa.Column(sa.TIMESTAMP())
+"""
+    ddl = """
+
+  CREATE TABLE "materials" (
+  "id" int PRIMARY KEY,
+  "title" varchar NOT NULL,
+  "description" varchar,
+  "link" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+  );
+
+  CREATE TABLE "material_attachments" (
+  "material_id" int,
+  "attachment_id" int,
+  CONSTRAINT "material_id_ibfk" FOREIGN KEY ("material_id") REFERENCES "materials" ("id"),
+  CONSTRAINT "attachment_id_ibfk" FOREIGN KEY ("attachment_id") REFERENCES "attachments" ("id")
+  );
+
+  CREATE TABLE "attachments" (
+  "id" int PRIMARY KEY,
+  "title" varchar,
+  "description" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+  );
+  """
+    result = create_models(ddl, models_type="sqlalchemy")["code"]
+    assert result == expected
+
+
+def test_multi_col_pk_and_fk():
+    """
+    This should test that we can properly setup tables with compound PRIMARY and
+    FOREIGN keys.
+    """
+    expected = """import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+
+
+Base = declarative_base()
+
+
+class Complexpk(Base):
+
+    __tablename__ = 'complexpk'
+
+    complex_id = sa.Column(int unsigned(), primary_key=True)
+    date_part = sa.Column(sa.DateTime(), server_default=func.now(), primary_key=True)
+    title = sa.Column(sa.String(), nullable=False)
+    description = sa.Column(sa.String())
+
+
+class LinkedTo(Base):
+
+    __tablename__ = 'linked_to'
+
+    id = sa.Column(sa.Integer(), primary_key=True)
+    complexpk_complex_id = sa.Column(sa.Integer(), sa.ForeignKey('complexpk.complex_id'))
+    complexpk_date_part = sa.Column(sa.Integer(), sa.ForeignKey('complexpk.date_part'))
+    comment = sa.Column(sa.String())
+"""
+
+    ddl = """
+
+  CREATE TABLE "complexpk" (
+  "complex_id" int unsigned NOT NULL,
+  "date_part" datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "title" varchar NOT NULL,
+  "description" varchar,
+  PRIMARY KEY ("complex_id","date_part")
+  );
+
+  CREATE TABLE "linked_to" (
+  "id" int PRIMARY KEY,
+  "complexpk_complex_id" int,
+  "complexpk_date_part" int,
+  "comment" varchar,
+  CONSTRAINT "id_date_part_ibfk" FOREIGN KEY ("complexpk_complex_id", "complexpk_date_part")
+    REFERENCES "complexpk" ("complex_id", "date_part")
+  );
+
+  """
+    result = create_models(ddl, models_type="sqlalchemy")["code"]
+    assert result == expected
+
+
 def test_upper_name_produces_the_same_result():
     expected = """import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
